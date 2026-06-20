8739a8e4e00ee74b630789ee510db1033844b792
saurav2306 <sauravjune2007@gmail.com>
2026-06-20 08:41:56 +0530

    Near By Hospital Api Integration

--- frontendDispensary/src/pages/Admin/NearByHospital/NearByModal/nearByModal.jsx
+++ frontendDispensary/src/pages/Admin/NearByHospital/NearByModal/nearByModal.jsx
@@ -1,7 +1,9 @@
-import React,{useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import './nearByModal.css'
+import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
 
-function NearByModal() {
+const NearByModal = (props) => {
     const [inputFields, setInputFields] = useState({
         name: "",
         address: "",
@@ -9,47 +11,72 @@ function NearByModal() {
     });
 
     const handleOnChange = (e, field) => {
-        setInputFields({...inputFields, [field]: e.target.value});
+        setInputFields({ ...inputFields, [field]: e.target.value });
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+     useEffect(() => {
+        if (props.clickedItem) {
+            setInputFields({ ...inputFields, name: props.clickedItem.name, address: props.clickedItem.address, contactNo: props.clickedItem.contactNo })
+        }
+    }, [])
+
+    const updateFunc = async () => {
+        await axios.put(`http://localhost:4000/api/hospital/update/${props.clickedItem._id}`, inputFields, { withCredentials: true }).then((resp) => {
+            window.location.reload();
+        }).catch(err => {
+            toast.error(err?.response?.data?.error)
+        })
     }
-  return (
-                <form className='register-form' onSubmit={handleSubmit}>
-                <div className='register-form-div'>
-                    <div className='register-input-box'> 
-                        <input 
-                            value={inputFields.name} 
-                            className='input-box-register' 
-                            type="text" 
-                            placeholder='Name' 
-                            onChange={(e) => handleOnChange(e, "name")} 
-                        />
-                    </div>
-                    <div className='register-input-box'> 
-                        <input 
-                            value={inputFields.address} 
-                            className='input-box-register' 
-                            type="text" 
-                            placeholder='Address' 
-                            onChange={(e) => handleOnChange(e, "address")} 
-                        />
-                    </div>
-                    <div className='register-input-box'> 
-                        <input 
-                            value={inputFields.contactNo} 
-                            className='input-box-register' 
-                            type="text" 
-                            placeholder='Contact No.' 
-                            onChange={(e) => handleOnChange(e, "contactNo")} 
-                        />
-                    </div>
-                    </div>
-                <button type='submit' className='form-btn reg-btn' >Add</button>
-                
-            </form>
-  )
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (inputFields.name.trim().length === 0 || inputFields.address.trim().length === 0 || inputFields.contactNo.trim().length === 0) return toast.error("Enter all the fields")
+        if (props.clickedItem){
+            updateFunc();
+            return;
+        }
+        await axios.post('http://localhost:4000/api/hospital/add', inputFields, { withCredentials: true }).then((resp) => {
+            window.location.reload();
+        }).catch(err => {
+            toast.error(err?.response?.data?.error)
+        })
+    }
+    return (
+        <form className='register-form' onSubmit={handleSubmit}>
+            <div className='register-form-div'>
+                <div className='register-input-box'>
+                    <input
+                        value={inputFields.name}
+                        className='input-box-register'
+                        type="text"
+                        placeholder='Name'
+                        onChange={(e) => handleOnChange(e, "name")}
+                    />
+                </div>
+                <div className='register-input-box'>
+                    <input
+                        value={inputFields.address}
+                        className='input-box-register'
+                        type="text"
+                        placeholder='Address'
+                        onChange={(e) => handleOnChange(e, "address")}
+                    />
+                </div>
+                <div className='register-input-box'>
+                    <input
+                        value={inputFields.contactNo}
+                        className='input-box-register'
+                        type="text"
+                        placeholder='Contact No.'
+                        onChange={(e) => handleOnChange(e, "contactNo")}
+                    />
+                </div>
+            </div>
+            <button type='submit' className='form-btn reg-btn' >{props.clickedItem ? "Update" : "Add"}</button>
+            <ToastContainer />
+        </form>
+    )
 }
 
 export default NearByModal
